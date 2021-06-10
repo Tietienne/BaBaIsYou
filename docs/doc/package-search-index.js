8fa00c471d0ab784f201f3735b0b3028104ac758
Etienne ALEXANDRE <tietienne77@gmail.com>
2021-06-10 19:30:48 +0200

    Added build.xml : compile, make jar, make javadoc

---
+++ docs/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"fr.umlv.zen5.demo"},{"l":"graphics"},{"l":"main"},{"l":"other"},{"l":"word"},{"l":"wordEnum"}];updateSearchResults();
